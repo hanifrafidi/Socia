4318824756a8799ef6aad493d6ef56e85e7d9979
Hanifrfd <50744311+hanifrafidi@users.noreply.github.com>
2022-05-27 18:41:48 +0700

    Update backend.js

--- src/backend.js
+++ src/backend.js
@@ -1,4 +1,4 @@
 export const server = {
-    url : 'http://localhost:4000',
-    // url : 'https://socia-apps.herokuapp.com',
+    // url : 'http://localhost:4000',
+    url : 'https://socia-apps.herokuapp.com',
 }
